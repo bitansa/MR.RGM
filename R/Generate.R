0e016bf9f3881acc458879d2fbfd960de74c2f39
Bitan Sarkar <bitansarkar010899@gmail.com>
2022-11-16 22:56:21 -0600

    Generate Rho based on Gamma matrix, p and hyper parameters a_rho, b_rho.

---
+++ R/Generate.R
@@ -0,0 +1,13 @@
+# Generate rho
+Generate_Rho = function(Gamma, p, a_rho, b_rho){
+
+  # Calculate sum of all entries of Gamma matrix
+  Gamma_sum = sum(Gamma)
+
+  # Generate Rho from Beta distribution
+  Rho = rbeta(1, Gamma_sum + a_rho, p * (p - 1) - Gamma_sum + b_rho)
+
+  # Return Rho
+  return(Rho)
+
+}
