78cc49df62940577a8d2f21e9e72db082a896f59
Bitan Sarkar <bitansarkar010899@gmail.com>
2023-06-30 01:53:59 -0500

    Update the wrapper function when Beta matrix is given as input

--- R/RGM.R
+++ R/RGM.R
@@ -4,11 +4,8 @@
 #'
 #'
 #' @param X n * k matrix data input, each row denotes a particular observation and each column denotes a particular DNA.
-#' @param Y n * p matrix data input, each row denotes a particular observation and each column denotes a particular gene.
-#' @param A0 p * p matrix input. It is the initial value of the gene-gene interaction matrix. Diagonal entries of A0 should all be 0. Random initialization of A0 may increase the convergence time of the markov chain. Default value is NULL.
-#' @param B0  p * k matrix input. It is the initial value of the gene-DNA interaction matrix. Each row corresponds to a particular gene, each column corresponds to a particular DNA. Random initialization of B0 matrix may increase the convergence time of the markov chain. Default value is NULL.
-#' @param D p * k indicator matrix input. Each row corresponds to a particular gene, each column corresponds to a particular DNA. An entry is 1 if the corresponding DNA can affect the corresponding gene, else 0. Default value is NULL. If not given, all entry of D is taken to be 1.
-#' @param a_tau positive scalar input. It corresponds to the first parameter of a Inverse Gamma distribution. Default value is 0.1.
+#' @param Y n * p matrix data input, each row denotes a particular observation and each column denotes a particular protein.
+#' @param d Vector input of length p. Each element is a non-negative integer corresponds to number of covariates affecting a particular protein, sum of which should be k.
 #' @param b_tau positive scalar input. It corresponds to the second parameter of a Inverse Gamma distribution. Default value is 0.1.
 #' @param a_rho positive scalar input. It corresponds to the first parameter of a Beta distribution. Default value is 0.5.
 #' @param b_rho positive scalar input. It corresponds to the first parameter of a Beta distribution. Default value is 0.5.
@@ -39,48 +36,45 @@
 #' # -----------------------------------------------------------------
 #' # Example 1:
 #'
-#' set.seed(500)
+#' set.seed(123)
 #'
 #' # Number of datapoints
-#' n = 500
+#' n = 10000
 #'
 #' # Number of Genes and number of DNAs
-#' p = 3
-#' k = 3
+#' p = 5
+#' k = 5
 #'
 #' # Initialize gene-gene interaction matrix
-#' A = matrix(sample(c(-3, 3), p^2, replace = TRUE), p, p)
+#' A = matrix(sample(c(-0.1, 0.1), p^2, replace = TRUE), p, p)
 #'
 #' # Diagonal entries of A matrix will always be 0
 #' diag(A) = 0
 #'
+#' # Make the network sparse
+#' A[sample(which(A!=0), length(which(A!=0))/2)] = 0
+#'
 #' # Initialize gene-DNA interaction matrix
-#' B = matrix(0, p, k)
+#' B = diag(p)
 #'
-#' for(i in 1:p){
-#'
-#'   B[i, i] = sample(c(-3, 3), 1, replace = TRUE)
-#'
-#' }
 #'
 #' # Indicator matrix for gene-DNA interaction
-#' D = diag(3)
+#' D = diag(p)
 #'
-#' Sigma = 0.5 * diag(p)
+#' Sigma = diag(p)
 #'
 #' Mult_Mat = solve(diag(p) - A)
 #'
 #' Variance = Mult_Mat %*% Sigma %*% t(Mult_Mat)
 #'
 #' # Generate DNA expressions
-#' X = matrix(runif(n * k, 0, 5), nrow = n, ncol = k)
+#' X = matrix(rnorm(n * k, 0, 1), nrow = n, ncol = k)
 #'
 #' Y = matrix(0, nrow = n, ncol = p)
 #'
-#' # Generate Gene expressions data based on DNA data
 #' for (i in 1:n) {
 #'
-#'  Y[i, ] = MASS::mvrnorm(1, Mult_Mat %*% B %*% X[i, ], Variance)
+#'     Y[i, ] = MASS::mvrnorm(n = 1, Mult_Mat %*% B %*% X[i, ], Variance)
 #'
 #' }
 #'
@@ -90,7 +84,7 @@
 #' S_XX = t(X) %*% X / n
 #'
 #' # Apply RGM on the generated data
-#' Out = RGM(S_YY, S_YX, S_XX, d = rep(1, 3), n = n)
+#' Out = RGM(S_YY, S_YX, S_XX, d = rep(1, p), n = n)
 #'
 #' # Get gene-gene interaction matrix, gene-DNA interaction matrix and log-likelihood
 #' A_Est = Out$A_Est
@@ -106,13 +100,7 @@
 #' \emph{Bayesian Analysis},
 #' \strong{13(4)}, 1095-1110.
 #' \doi{10.1214/17-BA1087}.
-RGM = function(S_YY, S_YX, S_XX, d, n, nIter = 10000, nBurnin = 2000, Thin = 1, prior = c("Threshold", "Spike and Slab"), a_tau = 0.01, b_tau = 0.01, a_rho = 0.5, b_rho = 0.5, nu_1 = 0.0001, a_eta = 0.01, b_eta = 0.01, a_psi = 0.5, b_psi = 0.5, nu_2 = 0.0001, a_sigma = 0.01, b_sigma = 0.01, Prop_VarA = 0.01, Prop_VarB = 0.01){
-
-  # Calculate number of nodes from S_YY matrix
-  p = ncol(S_YY)
-
-  # Calculate number of columns of S_XX matrix i.e. number of covariates
-  k = ncol(S_XX)
+RGM = function(X = NULL, Y = NULL, S_YY = NULL, S_YX = NULL, S_XX = NULL, Beta = NULL, Sigma_Hat = NULL, d, n, nIter = 10000, nBurnin = 2000, Thin = 1, prior = c("Threshold", "Spike and Slab"), a_tau = 0.01, b_tau = 0.01, a_rho = 0.5, b_rho = 0.5, nu_1 = 0.0001, a_eta = 0.01, b_eta = 0.01, a_psi = 0.5, b_psi = 0.5, nu_2 = 0.0001, a_sigma = 0.01, b_sigma = 0.01, Prop_VarA = 0.01, Prop_VarB = 0.01){
 
   # Check whether n is a positive integer
   if(!is.numeric(n) || n != round(n) || n <= 0){
@@ -122,7 +110,202 @@ RGM = function(S_YY, S_YX, S_XX, d, n, nIter = 10000, nBurnin = 2000, Thin = 1, 
 
   }
 
-  # Check whether d is a vector of non-negative integers of length p and sum od entries of d is equal to k
+  # Check whether input X and Y are given
+  if(!is.null(X) && !is.null(Y)){
+
+    # Check whether X and Y are both numeric matrices
+    if(!is.numeric(X) || !is.numeric(Y) || !is.matrix(X) || !is.matrix(Y)){
+
+      # Print an error message
+      stop("X and Y should be numeric matrices.")
+
+    }
+
+    # Check whether number of rows of X and Y are n
+    if(nrow(X) != n || nrow(Y) != n){
+
+      # Print an error message
+      stop("Number of rows of X and Y should be n.")
+
+    }
+
+    # Calculate number of nodes from Y matrix
+    p = ncol(Y)
+
+    # Calculate number of covariates from X matrix
+    k = ncol(X)
+
+    # Calculate S_YY, S_YX, S_XX
+    S_YY = t(Y) %*% Y / n
+    S_YX = t(Y) %*% X / n
+    S_XX = t(X) %*% X / n
+
+  } else if(!is.null(S_YY) && !is.null(S_YX) && !is.null(S_XX)){
+
+    # Check whether S_YY ,S_YX and S_XX are numeric matrices
+    if(!is.numeric(S_YY) || !is.numeric(S_YX) || !is.numeric(S_XX) || !is.matrix(S_YY) || !is.matrix(S_YX) || !is.matrix(S_XX)){
+
+      # Print an error message
+      stop("S_YY, S_YX and S_XX should be numeric matrices.")
+
+    }
+
+    # Calculate number of nodes from S_YY matrix
+    p = ncol(S_YY)
+
+    # Calculate number of covariates from S_XX matrix
+    k = ncol(S_XX)
+
+    # Check whether number of rows of S_YY is equal to p
+    if(nrow(S_YY) != p){
+
+      # Print an error message
+      stop("S_YY should be a diagonal matrix.")
+
+    }
+
+    # Check whether number of rows of S_XX is equal to k
+    if(nrow(S_XX) != k){
+
+      # Print an error message
+      stop("S_XX should be a diagonal matrix.")
+
+    }
+
+    # Check whether number of rows of S_YX is equal to p and number of columns of S_YX is equal to k
+    if(nrow(S_YX) != p || ncol(S_YX) != k){
+
+      # Print an error message
+      stop("Number of rows of S_YX should be equal to number of rows of S_YY and number of columns of S_YX should be equal to number of columns of S_XX.")
+
+    }
+
+
+
+  } else if(!is.null(S_XX) && !is.null(Beta) && !is.null(Sigma_Hat)){
+
+    # Check whether S_XX, Beta and Sigma_Hat are numeric matrices
+    if(!is.numeric(S_XX) || !is.numeric(Beta) || !is.numeric(Sigma_Hat) || !is.matrix(S_XX) || !is.matrix(Beta) || !is.matrix(Sigma_Hat)){
+
+      # Print an error message
+      stop("S_XX, Beta and SIgma_Hat should be numeric matrices.")
+
+    }
+
+    # Calculate number of nodes from Beta matrix
+    p = nrow(Beta)
+
+    # Calculate number of covariates from S_XX matrix
+    k = ncol(S_XX)
+
+    # Check whether number of rows of S_XX is equal to k
+    if(nrow(S_XX) != k){
+
+      # Print an error message
+      stop("S_XX should be a diagonal matrix.")
+
+    }
+
+    # Check whether number of columns of Beta is equal to k
+    if(ncol(Beta) != k){
+
+      # Print an error message
+      stop("Number of cloumns of Beta should be equal to number of columns of S_XX.")
+
+    }
+
+    # Check whether number of rows of Sigma_Hat is equal to p and number of columns of Sigma_Hat is equal to k
+    if(nrow(Sigma_Hat) != p || ncol(Sigma_Hat) != k){
+
+      # Print an error message
+      stop("Number of rows of Sigma_Hat should be equal to number of rows of Beta and number of columns of Sigma_Hat should be equal to number of columns of S_XX.")
+
+    }
+
+
+
+    # Calculate S_YX matrix
+    S_YX = t(t(Beta) *  diag(S_XX))
+
+    # Check whether d is a vector of non-negative integers of length p and sum od entries of d is equal to k
+    if(!is.numeric(d) || sum(d != round(d)) != 0 || sum(d <= 0) != 0 || length(d) != p || sum(d) != k){
+
+      # Print an error message
+      stop("d should be a vector of positive integers of length equal to number of nodes and sum of entries should be equal to number of covariates.")
+
+    }
+
+    # Store indices to extract particular columns from beta matrix
+    Col_Ind = c(0, cumsum(d))
+    Col_Ind = Col_Ind[-length(Col_Ind)] + 1
+
+    # Calculate Beta_New matrix by taking particular columns from Beta using Col_Ind
+    Beta_New = matrix(Beta[, Col_Ind], nrow = p, ncol = p)
+
+    # Initiate Estimate of A
+    A_Est = matrix(0, nrow = p, ncol = p)
+
+    # Update A_Est
+    for (i in 1:p) {
+
+      # Remove ith row and ith column of Beta_New
+      Beta_ii = Beta_New[-i, -i]
+      Beta_ii_det = det(Beta_ii)
+
+      for (j in 1:p) {
+
+        if(j != i){
+
+          # Remove jth row and ith column of Beta_New
+          beta_ji = Beta_New[-j, -i]
+
+          A_Est[i, j] = det(beta_ji) / Beta_ii_det * (-1)^(i + j + 1)
+
+        }
+
+      }
+
+    }
+
+    # Calculate (I-A_Est)^(-1)
+    Mult_Mat = solve(diag(p) - A_Est)
+
+    # Calculate diagonal entries of S_YY
+    S_YY_Diag = Sigma_Hat[, 1] + 2 * Beta[, 1] * S_YX[, 1] - Beta[, 1]^2 * S_XX[1, 1]
+
+    # Calculate correlation matrix between X and between Y and X
+    Cor_X = cov2cor(S_XX)
+    Cor_YX = t(t(Beta * (1 / sqrt(S_YY_Diag))) * sqrt(diag(S_XX)))
+
+    # Calculate determinant of Cor_X
+    Cor_X_Det = det(Cor_X)
+
+    # Calculate sum of squares of error while fitting Y[, i] on X
+    Error_Sumsq = rep(0, p)
+
+    for (i in 1:p) {
+
+      # Create R
+      R = cbind(c(1, Cor_YX[i,]), rbind(Cor_YX[i,], Cor_X))
+
+      # Update Error_Sumsq
+      Error_Sumsq[i] = S_YY_Diag[i] * det(R) / Cor_X_Det
+
+
+    }
+
+    # calculate Sigma for the model
+    Sigma = solve(Mult_Mat^2, Error_Sumsq)
+
+    # Calculate S_YY
+    S_YY = Beta %*% S_XX %*% t(Beta)  + Mult_Mat %*% (t(Mult_Mat) * Sigma)
+
+
+  }
+
+
+
+  # Check whether d is a vector of non-negative integers of length p and sum of entries of d is equal to k
   if(!is.numeric(d) || sum(d != round(d)) != 0 || sum(d <= 0) != 0 || length(d) != p || sum(d) != k){
 
     # Print an error message
